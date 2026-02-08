8661fcb83a6043ba5bf986da70b3d0e99510c43e
NewcomingDev9989 <kill3raaa@gmail.com>
2026-02-08 13:00:41 -0300

    ai que delicia o verão

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
- console.log("AIIIIIIIIIIIIIIINNNN DESGRAÇAAAAA TIRAAA QUE EU VOU CAGARR!!!!!");
+let nome = "Férigo o Sinestro"
+let nivelXP = 0
+function ganharXP() {
+    nivelXP += 7001
+    for (let i= 7001; i <= nivelXP; i++) {
+        console.log('Prabéns ' + nome + ', você avançou para o nível ' + i + '!')
+        
+    }
+}
