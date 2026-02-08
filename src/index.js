814187be31271551cd8dc49ecf00acb33cf20741
NewcomingDev9989 <kill3raaa@gmail.com>
2026-02-08 16:13:51 -0300

    tentativa 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 let nome = "Férigo o Sinestro"
-let nivelXP = 0
-function ganharXP() {
-    nivelXP += 7001
-    for (let i= 7001; i <= nivelXP; i++) {
-        console.log('Prabéns ' + nome + ', você avançou para o nível ' + i + '!')
-        
-    }
+let nivelXP = 8400
+let nivel= []
+ switch (nivelXP){
+    case nivelXP >= 0 && nivelXP <= 1000:
+        nivel.push("Ferro")
+        break
+    case nivelXP >= 1001 && nivelXP <= 2000:
+        nivel.push("Bronze")
+        break
+    case nivelXP >= 2001 && nivelXP <= 5000:
+        nivel.push("Prata")
+        break
+    case nivelXP >= 5001 && nivelXP <= 7000:
+        nivel.push("Ouro")
+        break
+    case nivelXP >= 7001 && nivelXP <= 8000:
+        nivel.push("Platina")
+        break
+    case nivelXP >= 8001 && nivelXP <= 9000:
+        nivel.push("Ascendente")
+        break
+    case nivelXP >= 9001 && nivelXP <= 10000:
+        nivel.push("imortal")
+        break
+    case nivelXP >= 10001:
+        nivel.push("lendário")
+        break
+    default:
+      console.log("Nível não reconhecido.")
+      
+for (let index = 0; index < nivel.length; index++) {
+    const element = nivel[index];
+    console.log(`Parabéns ${nome}, você tem ${nivelXP} de XP e esta no nível ${element}`)
 }
+  }
+  
+  
+
+                                  
